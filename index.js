296d9416ba4a91403e7b1af9f43ead1191862583
Raynos <raynos2@gmail.com>
2012-09-15 05:25:38 -0700

    also handle eventemitters

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var from = require("read-stream")
 module.exports = events
 
 function events(elem, eventName, capture) {
+    if (elem.addEventListener) {
+        return DOMEvents(elem, eventName, capture)
+    } else {
+        return EEEvents(elem, eventName)
+    }
+}
+
+function DOMEvents(elem, eventName, capture) {
     if (capture === undefined) {
         capture = false
     }
@@ -27,6 +35,39 @@ function events(elem, eventName, capture) {
     }
 }
 
+function EEEvents(ee, eventName) {
+    var buffer = []
+        , stream = from(read, end, buffer)
+
+    ee.on(eventName, addToBuffer)
+
+    return stream
+
+    function end() {
+        ee.removeListener(eventName, addToBuffer)
+        stream.emit("end")
+    }
+
+    function addToBuffer() {
+        if (arguments.length === 1) {
+            buffer.push(arguments[0])
+        } else {
+            buffer.push(toArray(arguments))
+        }
+        if (buffer.length === 1) {
+            stream.emit("readable")
+        }
+    }
+}
+
 function read(bytes, buffer) {
     return buffer.shift()
+}
+
+function toArray(list) {
+    var array = []
+    for (var i = 0; i < list.length; i++) {
+        array[i] = list[i]
+    }
+    return array
 }
